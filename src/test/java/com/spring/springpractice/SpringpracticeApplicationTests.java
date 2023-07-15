98d4dd617b49755e63b71bbfa13166a0c3f1af8f
SeoYeon Min <tjdus553926@gmail.com>
2023-07-15 17:01:46 +0900

    test

--- src/test/java/com/spring/springpractice/SpringpracticeApplicationTests.java
+++ src/test/java/com/spring/springpractice/SpringpracticeApplicationTests.java
@@ -1,15 +1,18 @@
 package com.spring.springpractice;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@EnableJpaAuditing
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringpracticeApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
