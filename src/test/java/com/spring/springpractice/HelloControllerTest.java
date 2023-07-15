216ec04df80e3421e9a1bcb28e6c747895962377
SeoYeon Min <tjdus553926@gmail.com>
2023-07-16 00:41:09 +0900

    test application.properties

--- src/test/java/com/spring/springpractice/HelloControllerTest.java
+++ src/test/java/com/spring/springpractice/HelloControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import static org.hamcrest.Matchers.is;
@@ -14,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+@WebAppConfiguration
 @RunWith(SpringRunner.class) // 스프링부트 테스트와 JUnit 사이에 연결자 역할을 함
 @WebMvcTest(controllers = HelloController.class) // Web(Spring MVC) 어노테이션에 집중할 수 있는 어노테이션. controller 관련 어노테이션 사용 가능
 public class HelloControllerTest {
